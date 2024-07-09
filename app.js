7914a650684f0dc500f8e0c23417c1cb865f689d
kimdahee0815 <billionairedahee@gmail.com>
2024-07-09 10:52:02 +0900

    fix: rate limit setting for trustProxy

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
+  validate: { trsutProxy: false },
 });
 app.use('/api', limiter);
 

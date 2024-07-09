d1bc42c1791017ab38a4f825229b711e631aad54
kimdahee0815 <billionairedahee@gmail.com>
2024-07-09 13:19:37 +0900

    refactor: imporve stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].price_data.unit_amount / 1000;
+  const price = session.object.amount_total / 1000;
   await Booking.create({ tour, user, price });
 });
 

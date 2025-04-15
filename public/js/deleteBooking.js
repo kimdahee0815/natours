4f4d1044f832d4e6a9872204596ee0c51229d0a5
kimdahee0815 <billionairedahee@gmail.com>
2025-04-15 20:45:21 +0900

    test: delete booking feature

--- public/js/deleteBooking.js
+++ public/js/deleteBooking.js
@@ -4,7 +4,10 @@ import { showAlert } from './alerts';
 
 export const deleteBooking = async (id) => {
   try {
-    const res = await axios.delete(`/api/v1/bookings/${id}`);
+    const res = await axios({
+      method: 'DELETE',
+      url: `/api/v1/bookings/${id}`,
+    });
 
     if (res.data.status === 'success') {
       showAlert('success', 'Your Booking was deleted successfully!');

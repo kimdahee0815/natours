2ce731f5827f01f2edd5bdbf03f997bee202edc2
kimdahee0815 <billionairedahee@gmail.com>
2025-04-17 18:05:30 +0900

    feat: billing table

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -98,9 +98,7 @@ exports.getUserBooking = catchAsync(async (req, res, next) => {
 
   return res.status(200).json({
     status: 'success',
-    data: {
-      paidBookings,
-    },
+    paidBookings,
   });
 });
 

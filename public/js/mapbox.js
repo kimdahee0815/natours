7d8b77235a0a8a6d4fcd5fc1cea7e6f97a19fa93
kimdahee0815 <billionairedahee@gmail.com>
2025-04-20 21:59:56 +0900

    feat: refactor create tour

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -13,7 +13,7 @@ export const getCoordinates = async function (address) {
     
     if (data.features && data.features.length > 0) {
       const [lng, lat] = data.features[0].center;
-      return `${lng},${lat}`;
+      return `${lat},${lng}`;
     }
     return '';
   } catch (err) {
